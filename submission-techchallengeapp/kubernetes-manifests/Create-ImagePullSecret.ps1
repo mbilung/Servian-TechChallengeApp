7d25a9ad8a5cc248d3cced533a3f958c7f3bf70a
mbilung <manish.bilung01@gmail.com>
2022-08-28 20:33:49 +0530

    added image pull secrets

---
+++ submission-techchallengeapp/kubernetes-manifests/Create-ImagePullSecret.ps1
@@ -0,0 +1,15 @@
+# Create an image pull secret
+# Prerequisite: create a namespace in AKS
+
+$jsonString = Get-Content -Path ../deployment/infrastructure/sp_aks.json
+$jsonObj = $jsonString | ConvertFrom-Json
+
+# service principal details to be used while AKS creation
+$aks_clientId = $jsonObj.appId
+$aks_clientSecret = $jsonObj.password
+
+kubectl create secret docker-registry acr-secret `
+--namespace servian `
+--docker-server=servianacr.azurecr.io `
+--docker-username=$aks_clientId `
+--docker-password=$aks_clientSecret
